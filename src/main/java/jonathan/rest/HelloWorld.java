8c1b2f7c989b0e45f039977e5ecc51d3d15c02be
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-02-14 11:59:33 -0300

    Status code

--- src/main/java/jonathan/rest/HelloWorld.java
+++ src/main/java/jonathan/rest/HelloWorld.java
@@ -9,5 +9,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
 		System.out.println(response.getBody().asString());
+		System.out.println(response.statusCode());
 	}
 }
