4bbe43d39ffe30385e3c4ff806a13da3da7591c2
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-02-14 15:38:26 -0300

    first test

--- src/main/java/jonathan/rest/HelloWorld.java
+++ src/main/java/jonathan/rest/HelloWorld.java
@@ -3,12 +3,17 @@ package jonathan.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
 
 public class HelloWorld {
 	
 	public static void main(String[] args) {
 		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-		System.out.println(response.getBody().asString());
-		System.out.println(response.statusCode());
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+		System.out.println(response.statusCode() == 200);
+		
+		 ValidatableResponse validacao = response.then();
+		 validacao.statusCode(200);
 	}
 }
