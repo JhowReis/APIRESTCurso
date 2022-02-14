92a70bad890c87bb978be2d8e9d1660c45734f80
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-02-14 11:06:46 -0300

    Hello World

---
+++ src/main/java/jonathan/rest/HelloWorld.java
@@ -0,0 +1,13 @@
+package jonathan.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+		System.out.println(response.getBody().asString());
+	}
+}
