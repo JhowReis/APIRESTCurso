57ea71d9947b921f9a0d9d65acc17fed21781046
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-02-15 17:44:56 -0300

    validando erro

--- src/main/java/jonathan/test/UserJsonTest.java
+++ src/main/java/jonathan/test/UserJsonTest.java
@@ -11,10 +11,8 @@ import io.restassured.http.Method;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
-
-
 public class UserJsonTest {
-	
+
 	@Test
 	public void deveVerificarPrimeiroNivel() {
 		given()
@@ -23,47 +21,43 @@ public class UserJsonTest {
 		.then()
 			.statusCode(200)
 			.body("id", is(1))
-			.body("name", is("João da Silva"))
-			.body("name", containsString("Silva"))
-			.body("age", is(30))
-			.body("age", greaterThan(18))
-			.body("salary", is(1234.5678F))
-		
+			.body("name", is("João da Silva")).body("name", containsString("Silva")).body("age", is(30))
+			.body("age", greaterThan(18)).body("salary", is(1234.5678F))
+
 		;
-		
+
 	}
-	
+
 	@Test
 	public void outrasFormasVerficarPrimeiroNivel() {
 		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/users/1");
-		
-		//PATH
-		Assert.assertEquals( 1, response.path("id"));
-		Assert.assertEquals( 1, response.path("%s","id"));
-		
-		//JSONPATH
+
+		// PATH
+		Assert.assertEquals(1, response.path("id"));
+		Assert.assertEquals(1, response.path("%s", "id"));
+
+		// JSONPATH
 		JsonPath jpath = new JsonPath(response.asString());
 		Assert.assertEquals(1, jpath.getInt("id"));
-		
-		//FROM
+
+		// FROM
 		int id = JsonPath.from(response.asString()).getInt("id");
 		Assert.assertEquals(1, id);
 	}
-	
+
 	@Test
 	public void deveVerificarSegundoNivel() {
 		given()
 		.when()
 			.get("https://restapi.wcaquino.me/users/2")
-		.then()
+		.then()	
 			.statusCode(200)
-			.body("name", containsString("Joaquina"))
-			.body("endereco.rua", is("Rua dos bobos"))
+			.body("name", containsString("Joaquina")).body("endereco.rua", is("Rua dos bobos"))
 			.body("endereco.numero", is(0))
 
 		;
 	}
-	
+
 	@Test
 	public void deveVerificarListas() {
 		given()
@@ -71,57 +65,23 @@ public class UserJsonTest {
 			.get("https://restapi.wcaquino.me/users/3")
 		.then()
 			.statusCode(200)
-			.body("name", containsString("Ana"))
-			.body("filhos", hasSize(2))
-			.body("filhos[0].name", is("Zezinho"))
-			.body("filhos[1].name", is("Luizinho"))
-			.body("filhos.name", hasItem("Luizinho"))
+			.body("name", containsString("Ana")).body("filhos", hasSize(2)).body("filhos[0].name", is("Zezinho"))
+			.body("filhos[1].name", is("Luizinho")).body("filhos.name", hasItem("Luizinho"))
 			.body("filhos.name", hasItems("Luizinho", "Zezinho"))
-
 
 		;
 	}
 
+	@Test
+	public void deveRetornarErroInexistente() {
+		given()
+		.when()
+			.get("https://restapi.wcaquino.me/users/4")
+		.then()
+			.statusCode(404)
+			.body("error", is("Usuário inexistente"));
+			
+		;		
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
