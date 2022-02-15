0909a33b7d7269df7ff6341324238fd3e6114c00
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-02-14 21:22:57 -0300

    Install Junit dependencies

--- src/main/java/jonathan/test/HelloWorld.java
+++ src/main/java/jonathan/test/HelloWorld.java
@@ -1,4 +1,4 @@
-package jonathan.rest;
+package jonathan.test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
---
+++ src/main/java/jonathan/test/HelloWorldTest.java
@@ -0,0 +1,22 @@
+package jonathan.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class HelloWorldTest {
+	
+	@Test
+	public void testHelloWorld() {
+		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		Assert.assertTrue(response.statusCode() == 200);
+		
+		 ValidatableResponse validacao = response.then();
+		 validacao.statusCode(200);
+	}
+}
