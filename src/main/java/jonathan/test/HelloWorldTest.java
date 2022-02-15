49cd0420313d9e790bfbbcb812cdb757c5b3736b
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-02-14 21:51:30 -0300

    Modo Fluente

--- src/main/java/jonathan/test/HelloWorldTest.java
+++ src/main/java/jonathan/test/HelloWorldTest.java
@@ -1,5 +1,9 @@
 package jonathan.test;
 
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,14 +13,30 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
 public class HelloWorldTest {
-	
+
 	@Test
 	public void testHelloWorld() {
-		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+		Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
 		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
 		Assert.assertTrue(response.statusCode() == 200);
+
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+	}
+
+	@Test
+	public void outrasFormarRestAssured() {
+		Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
+
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		get("https://restapi.wcaquino.me/ola").then().statusCode(200);
 		
-		 ValidatableResponse validacao = response.then();
-		 validacao.statusCode(200);
+		given()//pré condições
+		.when()//Ações
+			.get("https://restapi.wcaquino.me/ola")
+		.then()//Assertivas
+			.statusCode(200);
+
 	}
 }
