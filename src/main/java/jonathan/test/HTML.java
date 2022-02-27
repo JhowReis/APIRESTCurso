1bc61ffc78c3ee4bbf477066505074b65a2a65df
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-02-26 23:04:39 -0300

    busca com xpath e html

--- src/main/java/jonathan/test/HTML.java
+++ src/main/java/jonathan/test/HTML.java
@@ -1,9 +1,11 @@
 package jonathan.test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
+
+import io.restassured.http.ContentType;
 
 public class HTML {
 
@@ -21,6 +23,22 @@ public class HTML {
 			.body("html.body.div.table.tbody.tr[1].td[2]", is("25"))
 			.appendRootPath("html.body.div.table.tbody")
 			.body("tr.find{it.toString().startsWith('2')}.td[1]", is("Maria Joaquina"))
+		;
+	}
+	
+	@Test
+	public void deveFazerBuscAasComXpathEmHTML() {
+		given()
+		.log().all()
+		.when()
+		.get("https://restapi.wcaquino.me/v2/users?format=clean")
+		.then()
+		.log().all()
+		.statusCode(200)
+		.contentType(ContentType.HTML)
+		.body(hasXPath("count(//table/tr)", is("4")))
+		.body(hasXPath("//td[text() = '2']/../td[2]", is("Maria Joaquina")))
+		
 		;
 	}
 	
