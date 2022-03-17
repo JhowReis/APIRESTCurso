ff535f049733221f3516e05849e0e76aeec86d6a
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-03-17 16:41:37 -0300

    Deve baixar arquivo

--- src/main/java/jonathan/test/FileTest.java
+++ src/main/java/jonathan/test/FileTest.java
@@ -4,7 +4,12 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FileTest {
@@ -39,16 +44,35 @@ public class FileTest {
 	@Test
 	public void naoDeveFazerUploadArquivoGrande() {
 		given()	
-		.log().all()
-		.multiPart("arquivo", new File("src/main/resources/Resume-Jonathan-Reis.pdf"))
+			.log().all()
+			.multiPart("arquivo", new File("src/main/resources/Resume-Jonathan-Reis.pdf"))
 		.when()
-		.post("https://restapi.wcaquino.me/upload")
+			.post("https://restapi.wcaquino.me/upload")
 		.then()
-		.log().all()
-		.time(lessThan(5000L))
-		.statusCode(200)
+			.log().all()
+			.time(lessThan(5000L))
+			.statusCode(200)
 		;
 		
+	}
+	
+	@Test
+	public void deveBaixarArquivo() throws IOException {
+		byte[] image = given()	
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/download")
+		.then()
+//			.log().all()
+			.statusCode(200)
+			.extract().asByteArray()
+		;
+		File imagem = new File("src/main/resources/file.jpg");
+		OutputStream out = new FileOutputStream(imagem);
+		out.write(image);
+		out.close();
+		
+		Assert.assertThat(imagem.length(), lessThan(100000L));
 	}
 
 }
