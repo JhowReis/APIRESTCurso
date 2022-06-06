e21a037d7ebb5cd5da9c27d77a09bd2cee6c2636
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-06-06 20:22:46 -0300

    obtendo clima com api

--- src/main/java/jonathan/test/AuthTest.java
+++ src/main/java/jonathan/test/AuthTest.java
@@ -20,4 +20,26 @@ public class AuthTest {
 		
 		;
 	}
+	
+	@Test
+	public void deveObterClima() {
+		given()
+		.log().all()
+		.queryParam("q", "Recife,BR")
+		.queryParam("appid", "1d54ac0d1793b95a2868d7bde8db584e")
+		.queryParam("units", "metric")
+		.when()
+		.get("https://api.openweathermap.org/data/2.5/weather")
+		.then()
+		.log().all()
+		.statusCode(200)
+		.body("name", is("Recife"))
+		.body("coord.lon",is(-34.8811f))
+		
+		;
+	}
+	
+	
+	// 1d54ac0d1793b95a2868d7bde8db584e   https://api.openweathermap.org/data/2.5/weather?q=Recife,BR&appid=1d54ac0d1793b95a2868d7bde8db584e&units=metric
+
 }
