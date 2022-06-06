650f33cf04fe129635b13a8e9592894c1a921f5b
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-06-06 19:50:55 -0300

    acessando api publica

--- src/main/java/jonathan/test/AuthTest.java
+++ src/main/java/jonathan/test/AuthTest.java
@@ -1,5 +1,23 @@
 package jonathan.test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
 public class AuthTest {
 
+	@Test
+	public void deveAcessarSWAPI() {
+		given()
+			.log().all()
+		.when()
+			.get("https://swapi.dev/api/people/1")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Luke Skywalker"))
+		
+		;
+	}
 }
