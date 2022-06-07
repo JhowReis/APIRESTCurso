da8391060140e549a354a6d007248e2e372fc399
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2022-06-07 11:55:12 -0300

    Acessando aplicacao web

--- src/main/java/jonathan/test/AuthTest.java
+++ src/main/java/jonathan/test/AuthTest.java
@@ -9,6 +9,8 @@ import java.util.Map;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.XmlPath.CompatibilityMode;
 
 public class AuthTest {
 
@@ -132,6 +134,43 @@ public class AuthTest {
 		
 	}
 	
-
-
+	@Test
+	public void deveAcessarAplicacaoWeb() {
+		//login
+		String cookie = 
+				given()
+			.log().all()
+			.formParam("email", "jonathan.linkedin2019@gmail.com")
+			.formParam("senha", "jhowjhow2")
+			.contentType(ContentType.URLENC.withCharset("UTF-8"))
+		.when()
+			.post("http://seubarriga.wcaquino.me/logar")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.extract().header("set-cookie")
+			
+	;
+			cookie = cookie.split("=")[1].split(";")[0];
+			System.out.println("este é o cookie " + cookie);
+	
+	//obter conta
+	String body = 
+		given()
+			.log().all()
+			.cookie("connect.sid", cookie)
+		.when()
+			.get("http://seubarriga.wcaquino.me/contas")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("html.body.table.tbody.tr[1].td[0]",is("Conta mesmo nome"))
+			
+			.extract().body().asString()
+		;
+	
+		System.out.println("#################################");
+		XmlPath xmlpath = new XmlPath(CompatibilityMode.HTML, body);
+		System.out.println(xmlpath.getString("html.body.table.tbody.tr[1].td[0]"));
+	}
 }
